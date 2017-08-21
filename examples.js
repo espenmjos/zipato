14c69d60d9e1fe345eaf4f6ccd0bd70cfc2f6811
Espen Mjøs <espen.mjos@gmail.com>
2017-08-21 21:12:22 +0200

    Updated examples

--- examples.js
+++ examples.js
@@ -1,5 +1,24 @@
 var zipato = require('./index')
+var crypto = require('crypto')
+
+var password = "YOUR_PASSWORD"
+var username = "YOUR_USERNAME"
 
 zipato.user.initialize(function(err, resp, body){
-  console.log(body)
+  var nonce = body.nonce
+  var passwordHash = crypto.createHash('sha1').update(password).digest('hex')
+  var token = crypto.createHash('sha1').update(nonce + passwordHash).digest('hex')
+
+  let query = {
+    username: username,
+    token: token
+  }
+
+  zipato.user.login(query, authenticated)
 })
+
+function authenticated(err, resp, body){
+  zipato.devices.findAll(null, function(err, resp, body){
+    console.log(body)
+  })
+}
